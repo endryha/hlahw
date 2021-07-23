4d0ce7fa1aced0c2e8a924ee04aed8d997c02240
akovalov <akovalov@productiveedge.com>
2021-07-23 19:00:04 +0300

    merged read/write/top10 tests

--- 4_stress_test/siege/genUrlFiles.sh
+++ 4_stress_test/siege/genUrlFiles.sh
@@ -1,20 +1,15 @@
-write_urls_file=./input/write_urls.txt
-read_urls_file=./input/read_urls.txt
+urls_file=./input/urls.txt
 
-if [ -f "$write_urls_file" ]; then
-  rm "$write_urls_file"
+if [ -f "$urls_file" ]; then
+  rm "$urls_file"
 fi
 
-if [ -f "$read_urls_file" ]; then
-  rm "$read_urls_file"
-fi
-
-touch $write_urls_file
-touch $read_urls_file
+touch $urls_file
 
 for ((i = 0; i < $1; i++)); do
   uuid=$(uuidgen)
   n=$RANDOM
-  echo "http://localhost:8082/write POST uuid=$uuid&value=$n" >>$write_urls_file
-  echo "http://localhost:8082/get?uuid=$uuid" >>$read_urls_file
+  echo "http://localhost:8082/write POST uuid=$uuid&value=$n" >>$urls_file
+  echo "http://localhost:8082/get?uuid=$uuid" >>$urls_file
+  echo "http://localhost:8082/top10" >>$urls_file
 done
--- 4_stress_test/siege/run.sh
+++ 4_stress_test/siege/run.sh
@@ -1,13 +1,10 @@
-write_urls_file=./input/write_urls.txt
-read_urls_file=./input/read_urls.txt
+urls_file=./input/urls.txt
 
-write_stats_file=./output/write_stats.txt
-read_stats_file=./output/read_stats.txt
-top10_stats_file=./output/top10_stats.txt
+stats_file=./output/stats.txt
 
 concurrency=$1
 duration=$2
-pause=3
+pause=1
 
 echo "Concurrency: $concurrency"
 echo "Duration: $duration"
@@ -15,28 +12,10 @@ echo "Duration: $duration"
 echo "Generating url files"
 ./genUrlFiles.sh 1000
 
-echo "Start write stress test"
+echo "Start stress test"
 sleep $pause
 
-siege -c"$concurrency" -t"$duration" -f $write_urls_file 2>$write_stats_file &
+siege -i -c"$concurrency" -t"$duration" -f $urls_file 2>$stats_file
 
-sleep $pause
-echo "Start read stress test"
-sleep $pause
-
-siege -c"$concurrency" -t"$duration" -f $read_urls_file 2>$read_stats_file &
-
-sleep $pause
-echo "Start top 10 stress test"
-sleep $pause
-
-siege -c"$concurrency" -t"$duration" http://localhost:8082/top10 2>$top10_stats_file &
-
-echo "Write Stats"
-cat $write_stats_file
-
-echo "Read Stats"
-cat $read_stats_file
-
-echo "Top 10 Stats"
-cat top10_stats_file
+echo "Result"
+cat $stats_file
