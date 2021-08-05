926f066900c23e9957f9218b7294add7145f3611
akovalov <akovalov@productiveedge.com>
2021-08-05 22:43:58 +0300

    Nginx cache server

---
+++ 6_nginx_image_cache/scripts/cache_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for path in ../images/*; do
+    file="$(basename -- $path)"
+    echo -n "$path -> $file -> "
+    echo "curl -s -o /dev/null http://localhost:8081/images/$file"
+    curl -s -o /dev/null "http://localhost:8081/images/$file"
+done
