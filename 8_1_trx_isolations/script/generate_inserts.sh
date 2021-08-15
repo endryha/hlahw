b4942f6b49d4307df6730eb2254a141a4e0713fe
akovalov <akovalov@productiveedge.com>
2021-08-15 17:12:48 +0300

    added schema to docker

--- 8_1_trx_isolations/script/generate_inserts.sh
+++ 8_1_trx_isolations/script/generate_inserts.sh
@@ -1,4 +1,4 @@
 count=$1
 for i in $(seq $count); do
-    echo "INSERT INTO users (name, age) VALUES ('$(faker name)', $(( ( RANDOM % 80 )  + 18 )));"
+    echo "INSERT INTO users (name, age) VALUES ('$(faker name)', $(( ( RANDOM % 50 )  + 18 )));"
 done
--- 8_1_trx_isolations/sql/schema.sql
+++ 8_1_trx_isolations/sql/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users
 (
-    id   int(11) NOT NULL AUTO_INCREMENT,
+    id   int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(50) NOT NULL,
     age  int(3) NOT NULL,
-    PRIMARY KEY (id)
+    INDEX USING BTREE (age)
 );
 
-CREATE INDEX name_idx ON users (name) USING BTREE;
-CREATE INDEX age_idx ON users (age) USING BTREE;
+-- CREATE INDEX name_idx ON users (name) USING BTREE;
+-- CREATE INDEX age_idx ON users (age) USING BTREE;
